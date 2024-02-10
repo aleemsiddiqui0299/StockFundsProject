d6a583479e342a60edba3390a251b80519b9f645
aleemsiddiqui <aleemsiddiqui029@gmail.com>
2024-02-10 20:29:27 +0530

    added resolvers for queries

--- backend/schema.js
+++ backend/schema.js
@@ -26,6 +26,11 @@ resolvers: {
     Equity: {
         //logic to grab equity based on 
         company:async (equity)=> await Equities.find({ $company_id: equity.company_id})
+    },
+    Query: {
+        getAllEquities : async ()=> await Equities.find({}),
+        getAllCompanies : async ()=> await Companies.find({}),
+        getEquity: async(parent, {id}): await Equities.find({ $company_id: equity.company_id}),
     }
 }
 //{"name":"TCS" ,category: "IT", "company_id": 101, "open_value": 123.5, "close_value": 125.1},
