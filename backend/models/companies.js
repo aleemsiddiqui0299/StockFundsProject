37477101619f43c8f0c732158e3879d97dd8638d
aleemsiddiqui0299 <86101375+aleemsiddiqui0299@users.noreply.github.com>
2024-02-16 23:38:32 +0530

    cleaned code

--- backend/models/companies.js
+++ backend/models/companies.js
@@ -12,5 +12,3 @@ const companiesSchema = new mongoose.Schema(
 const Companies = mongoose.model('Companies', companiesSchema);
 
 module.exports ={ Companies }
-//{"name":"TCS" ,category: "IT", "company_id": 101, "open_value": 123.5, "close_value": 125.1},
-//{ "companyId":101, "name":"Tata Group" , "year":1968 }, 
