983d00817c48f4ff2a4589cde31d64bb5484f8d4
aleemsiddiqui0299 <86101375+aleemsiddiqui0299@users.noreply.github.com>
2024-02-16 23:36:30 +0530

    used components of stocks and companies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-// import CompanyList from './components/companyList/CompanyList';
-// import StockList from './components/stockList/StockList';
+import CompanyList from './components/companyList/CompanyList';
+import StockList from './components/stockList/StockList';
 
 function App() {
   return (
     <div className="App">
       <h1>List of All Stocks and Companies</h1>
+    <StockList />
+    <h1>Companies List</h1>
+    <CompanyList />
     </div>
   )
 }
