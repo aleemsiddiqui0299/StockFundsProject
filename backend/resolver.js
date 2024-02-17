798916ca895877549c54c028b2e5f3c837d33cab
aleemsiddiqui0299 <86101375+aleemsiddiqui0299@users.noreply.github.com>
2024-02-17 07:59:08 +0530

    formatted resolvers code

--- backend/resolver.js
+++ backend/resolver.js
@@ -1,26 +1,18 @@
 const {Equities} = require('./models/products')
 const {Companies} = require('./models/companies')
 const mongoose = require('mongoose');
-
-
 const resolvers= {
     Equity: {
-
-        //logic to grab equity based on 
         company: async function(parent) {
-
             console.log('Parent equity name : '+ parent.name+" equity company id : ", parent.company_id);
             const equity = await Companies.findOne({ companyId: parent.company_id});
             console.log('Result for company in equity : ', equity);
             return equity;
-
         }
     },
     Query: {
-
         getAllEquities : async ()=> await Equities.find(),
         getAllCompanies : async ()=> await Companies.find(),
-
         getEquity: async function(parent, {name}) {
             try{
                 console.log("RECEIVED name for equity search: ", name);
@@ -31,7 +23,6 @@ const resolvers= {
                 console.log("Database Error: ", error);
             } 
         },
-        
         getCompany: async function(parent, {companyId}) {
             try{
                 console.log("RECEIVED Company_id for Company search: ", companyId);
